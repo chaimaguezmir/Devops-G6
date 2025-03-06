b15e9d9a4a86f7274695301e8f1d5f4a83a44fee
Anas <anas.bettouzia@esprit.tn>
2025-03-06 10:13:25 +0100

    testinstructor avec crud

--- src/test/java/tn/esprit/spring/services/InstructorServicesImplTest.java
+++ src/test/java/tn/esprit/spring/services/InstructorServicesImplTest.java
@@ -76,58 +76,7 @@ class InstructorServicesImplTest {
         assertEquals(1L, retrievedInstructor.getNumInstructor());
     }
 
-    @Test
-    void addInstructorAndAssignToCourse() {
-        Instructor instructor = new Instructor();
-        instructor.setNumInstructor(1L);
-        Course course = new Course();
-        course.setNumCourse(1L);
 
-        when(courseRepository.findById(1L)).thenReturn(Optional.of(course));
-        when(instructorRepository.save(any(Instructor.class))).thenAnswer(invocation -> invocation.getArgument(0));
-
-        Instructor savedInstructor = instructorServices.addInstructorAndAssignToCourse(instructor, 1L);
-
-        assertNotNull(savedInstructor);
-        assertEquals(1, savedInstructor.getCourses().size());
-        assertTrue(savedInstructor.getCourses().contains(course));
-    }
-    @Test
-    void removeCourseFromInstructor() {
-        Instructor instructor = new Instructor();
-        instructor.setNumInstructor(1L);
-        Course course = new Course();
-        course.setNumCourse(1L);
-
-        instructor.setCourses(new HashSet<>(List.of(course)));
-
-        when(instructorRepository.findById(1L)).thenReturn(Optional.of(instructor));
-        when(courseRepository.findById(1L)).thenReturn(Optional.of(course));
-        when(instructorRepository.save(any(Instructor.class))).thenAnswer(invocation -> invocation.getArgument(0));
-
-        Instructor updatedInstructor = instructorServices.removeCourseFromInstructor(1L, 1L);
-
-        assertNotNull(updatedInstructor);
-        assertEquals(0, updatedInstructor.getCourses().size());
-    }
-    @Test
-    void getCoursesByInstructor() {
-        Instructor instructor = new Instructor();
-        instructor.setNumInstructor(1L);
-        Course course1 = new Course();
-        course1.setNumCourse(1L);
-        Course course2 = new Course();
-        course2.setNumCourse(2L);
-
-        instructor.setCourses(new HashSet<>(List.of(course1, course2)));
-
-        when(instructorRepository.findById(1L)).thenReturn(Optional.of(instructor));
-
-        Set<Course> courses = instructorServices.getCoursesByInstructor(1L);
-
-        assertNotNull(courses);
-        assertEquals(2, courses.size());
-    }
 
 
 }
